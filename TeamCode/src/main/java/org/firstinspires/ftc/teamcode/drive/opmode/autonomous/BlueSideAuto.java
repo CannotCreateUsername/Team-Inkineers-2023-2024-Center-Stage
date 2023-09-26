b465a55e958fd185b205ba912ba3a7ec33afe1ea
CannotCreateUsername <peckerisawesome@outlook.com>
2023-09-25 21:29:08 -0700

    Roadrunner dead wheel config, install FTClib and EasyOpenCV libraries

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/BlueSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/BlueSideAuto.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.autonomous;
 
-public class BlueSideAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Blue Alliance Auto", group = "Linear Opmode")
+public class BlueSideAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
 }
