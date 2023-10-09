707023a36852412e8eca856594d7fcb4dd2a2dd3
CannotCreateUsername <peckerisawesome@outlook.com>
2023-10-08 19:37:47 -0700

    Intake subsystem and intialization in teleOP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/IntakeSubsystem.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import androidx.annotation.NonNull;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class IntakeSubsystem {
+    public enum IntakeBarState {
+        FOLDED,
+        UNFOLDED
+    }
+    public enum IntakeState {
+        IDLE,
+        IN,
+        OUT
+    }
+
+    double Kp = .05;
+
+    public DcMotor intakeBar = null;
+    public CRServo intakeWheel = null;
+
+    IntakeBarState intakeBarState;
+    IntakeState intakeState;
+
+
+    double currentTarget;
+
+    public void init(@NonNull HardwareMap hardwareMap) {
+        intakeBar = hardwareMap.get(DcMotor.class, "intakeBar");
+        intakeBar.setDirection(DcMotorSimple.Direction.REVERSE);
+        intakeBar.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeBar.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeBar.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        intakeWheel = hardwareMap.get(CRServo.class, "intakeWheel");
+        intakeBarState = IntakeBarState.FOLDED;
+        intakeState = IntakeState.IDLE;
+    }
+
+    public double powerPID(double power) {
+        if (Math.abs(intakeBar.getCurrentPosition() - currentTarget) > 15){
+            // our threshold is within
+            // 15 encoder ticks of our target.
+            // this is pretty arbitrary, and would have to be
+            // tweaked for each robot.
+            return power;
+        } else {
+            double posErr = currentTarget - intakeBar.getCurrentPosition(); // measure error in terms of distance between current position and target
+            return (posErr * Kp); //instead of fixed power, use the concept of PID and increase power in proportion with the error
+        }
+    }
+
+    public void runIntake(GamepadEx gamepad1) {
+        TriggerReader rtReader = new TriggerReader(gamepad1, GamepadKeys.Trigger.RIGHT_TRIGGER);
+        TriggerReader ltReader = new TriggerReader(gamepad1, GamepadKeys.Trigger.LEFT_TRIGGER);
+        switch (intakeState) {
+            case IDLE:
+                intakeWheel.setPower(0);
+                if (rtReader.isDown()) {
+                    intakeState = IntakeState.IN;
+                } else if (ltReader.isDown() && !rtReader.isDown()) {
+                    intakeState = IntakeState.OUT;
+                }
+            case IN:
+                intakeWheel.setPower(1);
+                if (rtReader.wasJustReleased()) {
+                    intakeState = IntakeState.IDLE;
+                }
+            case OUT:
+                intakeWheel.setPower(-1);
+                if (ltReader.wasJustReleased()) {
+                    intakeState = IntakeState.IDLE;
+                }
+        }
+    }
+
+    public void runIntakeLift(GamepadEx gamepad1) {
+        switch (intakeBarState) {
+            case FOLDED:
+                if (gamepad1.wasJustPressed(GamepadKeys.Button.A)) {
+                    runToPosition(-200);
+                    intakeBarState = IntakeBarState.UNFOLDED;
+                }
+            case UNFOLDED:
+                if (gamepad1.wasJustPressed(GamepadKeys.Button.B)) {
+                    runToPosition(0);
+                    intakeBarState = IntakeBarState.FOLDED;
+                }
+        }
+    }
+
+    public void runToPosition(int position, double power) {
+        currentTarget = position;
+        intakeBar.setTargetPosition(position);
+        intakeBar.setPower(powerPID(power));
+    }
+
+    // Run to position at default power
+    public void runToPosition(int position) {
+        runToPosition(position, 0.5);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/BlueSideDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/BlueSideDrive.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.IntakeSubsystem;
 
 @TeleOp(name = "Whats under there", group = "Linear Opmode")
 public class BlueSideDrive extends LinearOpMode {
@@ -15,9 +17,13 @@ public class BlueSideDrive extends LinearOpMode {
         GamepadEx gamepadEx1 = new GamepadEx(gamepad1);
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(0)));
+        IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+        intakeSubsystem.init(hardwareMap);
 
         waitForStart();
         while (opModeIsActive()) {
+            Pose2d poseEstimate = drive.pose;
+
             telemetry.addData("DEEZ NUTS", "under-where!? xdddDDddddd lol.");
             telemetry.update();
         }
