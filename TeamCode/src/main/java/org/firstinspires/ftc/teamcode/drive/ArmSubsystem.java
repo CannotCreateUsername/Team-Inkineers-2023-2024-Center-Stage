180c023abf69bff4a495511d8710f33bbbafdca1
KiwiPheonix <CannotCreateUsername>
2023-11-08 12:24:44 -0800

    V4b, outtake, and Vslides FSMs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ArmSubsystem.java
@@ -6,13 +6,17 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class ArmSubsystem {
     public enum SlideState {
+        READY,
         RUNNING,
         PAUSED,
         REST
@@ -21,31 +25,50 @@ public class ArmSubsystem {
         DROP_OFF,
         PICK_UP
     }
+    public enum OuttakeState {
+        OUT,
+        IN,
+        IDLE
+    }
 
     private final static double Kp = .05;
 
-    private DcMotor slides = null;
-    private Servo virtualBar = null;
+    private final DcMotor slides;
+    private final Servo virtualBar;
+    private final CRServo outtake;
 
     public final int DROP = 1;
     public final int LOAD = 0;
 
     SlideState slideState;
     DropState dropState;
+    OuttakeState outtakeState;
 
     double currentTarget;
 
+    ElapsedTime timer;
+
+    TriggerReader rtReader;
+    TriggerReader ltReader;
+
     public ArmSubsystem(HardwareMap hardwareMap) {
+        // Map actuator variables to actual hardware
         slides = hardwareMap.get(DcMotor.class, "slides");
+        virtualBar = hardwareMap.get(Servo.class, "bar");
+        outtake = hardwareMap.get(CRServo.class, "outtake");
+
+        // Motor behavior setup
         slides.setDirection(DcMotorSimple.Direction.REVERSE);
         slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        virtualBar = hardwareMap.get(Servo.class, "bar");
-
+        // Initialize finite state machines
         slideState = SlideState.REST;
         dropState = DropState.PICK_UP;
+        outtakeState = OuttakeState.IDLE;
+
+        timer = new ElapsedTime();
     }
 
     public double powerPID(double power) {
@@ -61,43 +84,79 @@ public class ArmSubsystem {
         }
     }
 
-    public void runSlides(GamepadEx gamepad1) {
+    public void runArm(GamepadEx gamepad1) {
         switch (slideState) {
             case REST:
-                runToPosition(0);
+                if (gamepad1.isDown(GamepadKeys.Button.RIGHT_BUMPER)) {
+                    runToPosition(100);
+                    slideState = SlideState.READY;
+                    timer.reset();
+                }
+                break;
+            case READY:
                 if (gamepad1.isDown(GamepadKeys.Button.RIGHT_BUMPER)) {
                     slideState = SlideState.RUNNING;
+                } else if (gamepad1.wasJustReleased(GamepadKeys.Button.LEFT_BUMPER)) {
+                    virtualBar.setPosition(LOAD);
+                    runToPosition(0);
+                    slideState = SlideState.REST;
+                    timer.reset();
                 }
+                if (timer.seconds() > 1) {
+                    virtualBar.setPosition(DROP);
+                }
+                break;
             case RUNNING:
-                runToPosition(slides.getCurrentPosition()+5);
+                runToPosition(slides.getCurrentPosition()+2);
                 if (gamepad1.wasJustReleased(GamepadKeys.Button.RIGHT_BUMPER)) {
                     slideState = SlideState.PAUSED;
                 } else if (gamepad1.wasJustReleased(GamepadKeys.Button.LEFT_BUMPER)) {
+                    virtualBar.setPosition(LOAD);
+                    runToPosition(0);
                     slideState = SlideState.REST;
+                    timer.reset();
                 }
+                break;
             case PAUSED:
                 runToPosition(slides.getCurrentPosition());
-                if (gamepad1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+                if (gamepad1.isDown(GamepadKeys.Button.RIGHT_BUMPER)) {
+                    slideState = SlideState.RUNNING;
+                } else if (gamepad1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+                    virtualBar.setPosition(LOAD);
+                    runToPosition(0);
                     slideState = SlideState.REST;
+                    timer.reset();
                 }
+                break;
         }
     }
 
-    public void runFourBar(GamepadEx gamepad1) {
-        switch (dropState) {
-            case PICK_UP:
-                virtualBar.setPosition(LOAD);
-                if (gamepad1.isDown(GamepadKeys.Button.X)) {
-                    dropState = DropState.DROP_OFF;
+    public void runOuttake(GamepadEx gamepad2) {
+        rtReader = new TriggerReader(gamepad2, GamepadKeys.Trigger.RIGHT_TRIGGER);
+        ltReader = new TriggerReader(gamepad2, GamepadKeys.Trigger.LEFT_TRIGGER);
+        switch (outtakeState) {
+            case IDLE:
+                outtake.setPower(0);
+                if (ltReader.isDown()) {
+                    outtakeState = OuttakeState.OUT;
+                } else if (rtReader.isDown()) {
+                    outtakeState = OuttakeState.IN;
                 }
-            case DROP_OFF:
-                virtualBar.setPosition(DROP);
-                if (gamepad1.wasJustReleased(GamepadKeys.Button.X)) {
-                    dropState = DropState.PICK_UP;
+                break;
+            case IN:
+                outtake.setPower(1);
+                if (ltReader.wasJustReleased()) {
+                    outtakeState = OuttakeState.IDLE;
                 }
+                break;
+            case OUT:
+                outtake.setPower(-1);
+                if (rtReader.wasJustReleased()) {
+                    outtakeState = OuttakeState.IDLE;
+                }
+                break;
         }
     }
-
 
     public void runToPosition(int position, double power) {
         currentTarget = position;
@@ -110,6 +169,7 @@ public class ArmSubsystem {
         runToPosition(position, 0.5);
     }
 
+    // Telemetry
     public String getLiftState() { return slideState.name(); }
     public String getLoadState() { return dropState.name(); }
 
