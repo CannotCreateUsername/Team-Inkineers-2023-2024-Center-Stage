7fcf2c99ef8211cd921315bd608dd45f19726990
KiwiPheonix <CannotCreateUsername>
2024-02-20 22:30:09 -0800

    retuned for purple pixel (lateralinpertick)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -50,8 +50,8 @@ public final class MecanumDrive {
     public static class Params {
         // drive model parameters
         public double inPerTick = 0.00295700689202494256645881194618; //Avg Ticks: 30470 29800.333 Distance: 90 inches (Meet 0)
-        public double lateralInPerTick = 0.001832293990833905;
-        public double trackWidthTicks = 3994.2774019102985; // 3603.57260358197 (Meet 0)
+        public double lateralInPerTick = 0.0023;
+        public double trackWidthTicks = 3957.886882296457; // 3603.57260358197 (Meet 0)
 
         // feedforward parameters (in tick units)
         public double kS = 0.2; // 0.07 (Meet 0)
@@ -69,11 +69,11 @@ public final class MecanumDrive {
 
         // path controller gains
         public double axialGain = 3.0;
-        public double lateralGain = 0.1;
-        public double headingGain = 2.2; // shared with turn
+        public double lateralGain = 0;
+        public double headingGain = 1; // shared with turn
 
-        public double axialVelGain = 0.05;
-        public double lateralVelGain = 0;
+        public double axialVelGain = 2.5;
+        public double lateralVelGain = 0.1;
         public double headingVelGain = 0.1; // shared with turn
     }
 
